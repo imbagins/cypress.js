44ef56d44d3d868b4e608f33eb45735cc9e5ea24
Darya Meshkova <ugoslavi13@mail.ru>
2022-11-23 20:11:33 +0300

    Update meshkova_reg_pos.js

--- meshkova_reg_pos.js
+++ meshkova_reg_pos.js
@@ -3,8 +3,8 @@ describe('Тестирование сайта staya', function () {
     it('Заходим на сайт и авторизуемся, далее - проверяем авторизацию', function () {
         cy.visit('https://staya.dog/');
         cy.get('.header-bottom__right--link').click();
-        cy.get('.auth-form > form > [type="email"]').type('ugoslavi13@mail.ru');
-        cy.get('.auth-form > form > [type="password"]').type('Qwedcvb1');
+        cy.get('.auth-form > form > [type="email"]').type('example@mail.ru');
+        cy.get('.auth-form > form > [type="password"]').type('верный пароль');
         cy.get('.auth-form__submit > .s-button__content').click();
         cy.contains('Мои заказы');
        
